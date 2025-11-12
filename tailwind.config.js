bd1540b7d73bbad208a2df0cc7da4558e6509483
albertlazarus <albertlazarus@connectinglegacy.com>
2025-11-12 11:14:39 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        legacyBlue: "#004AAD",
+        legacyYellow: "#FFD700"
+      },
+    },
+  },
+  plugins: [],
+};
