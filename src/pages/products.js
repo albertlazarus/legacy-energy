7dc931f2fe1c5dea310dffa187bfa68d32cc150c
albertlazarus <albertlazarus@connectinglegacy.com>
2025-11-13 13:15:58 +0530

    Update products.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,5 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import ProductCard from "@/components/ProductCard";
 
 const products = [
